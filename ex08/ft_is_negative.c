303376b4556752c66d1e0e368ecccf615a68cc36
Adriana Chiquito <144694058+adrianacp10@users.noreply.github.com>
2024-12-18 14:59:47 +0000

    ejercicios corregidos menos el 19/20 y 0/5

--- ex08/ft_is_negative.c
+++ ex08/ft_is_negative.c
@@ -1,12 +1,7 @@
-#include <unistd.h>
+void    ft_putchar(char c);
 
-void    ft_putchar(char c)
+void   ft_is_negative(int n)
 {
-    write(1, &c, 1);
-}
-
- void   ft_is_negative(int n)
- {
     if (n < 0)
     {
         ft_putchar('N');
@@ -15,7 +10,7 @@ void    ft_putchar(char c)
     {
         ft_putchar('P');
     }
- }
+}
 /*
  int    main()
  {
--- ex10/ft_swap.c
+++ ex10/ft_swap.c
@@ -1,7 +1,21 @@
- void   ft_swap(int *a, int *b)
- {
-    int c;
-    *a = c;
-    *b = *a;
-    *a = *b;
- }
+void  ft_swap(int *a, int *b)
+{
+   int c;
+   c = *a;
+   *a = *b;
+   *b = c;
+}
+/*
+int   main()
+{
+   int a;
+   int b;
+
+   a = 10;
+   b = 20;
+
+   printf("valor de a: %d y de b: %d\n", a, b);
+   ft_swap(&a, &b);
+   printf("despues a: %d y b: %d", a, b);
+   return(0);
+}*/
